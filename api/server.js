f994e68d5290fd2f9da7e687de00d21fa575a701
Ross J <ross.joo@gmail.com>
2019-06-26 22:31:12 -0400

    Format and flesh out podcast content drawer / episodes list

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const https = require('https');
 const cors = require('cors');
 const app = express();
 const X2JS = require('x2js');
+const sanitizeHtml = require('sanitize-html');
 
 app.use(express.json());
 app.use(cors());
@@ -25,8 +26,8 @@ app.post('/api/feed', cors(), (request, response) => {
     res.on('data', (d) => data += d);
     res.on('end', () => {
       try {
-        const parsed = x2js.xml2js(data);
-        setTimeout(() => response.json(parsed), 5000);
+        const parsed = formatOutput(x2js.xml2js(data));
+        setTimeout(() => response.json(parsed), 2000);
       } catch(e) {
         response.status(500).json({ error: 'Error converting feed to json', log: e.message });
       }
@@ -35,5 +36,75 @@ app.post('/api/feed', cors(), (request, response) => {
     response.status(500).json({ error: 'Unable to retrieve feed', log: err.message });
   });
 });
+
+/**
+ * Quick/dirty formatting of some discrepancies
+ * This is in no way comprehensive
+ */
+function formatOutput(data) {
+  const output = {
+    raw: data
+  };
+
+  output.title = data.rss.channel.title;
+  output.episodes = data.rss.channel.item.map(ep => {
+    const episode = {
+      id: ep.guid,
+      link: ep.link,
+      media: '',
+      pubDate: ep.pubDate,
+      summary: '',
+      title: ep.title,
+    };
+
+    // Find proper ID
+    ['__text', '__cdata'].forEach(attr => {
+      if (ep.guid.hasOwnProperty(attr))
+        episode.id = ep.guid[attr];
+    });
+
+    if (Array.isArray(ep.title)) {
+      episode.title = ep.title.find(t => typeof t === 'string');
+    } else if (ep.title.hasOwnProperty('__text')) {
+      episode.title = ep.title.__text;
+    }
+
+    // @todo
+    // episode.link
+    // episode.media
+    // episode.summary
+
+    return episode;
+  });
+
+  const img = data.rss.channel.image;
+
+  output.image = null;
+  if (Array.isArray(img)) {
+    output.image = img.find(i => i.hasOwnProperty('_href'))._href;
+  } else if (img._href) {
+    output.image = img._href;
+  } else if (img.url) {
+    output.image = img.url;
+  }
+
+  const desc = data.rss.channel.description;
+
+  output.description = '';
+  if (Array.isArray(desc)) {
+    output.description = desc.find(d => typeof d === 'string');
+
+    if (!output.description) {
+      output.description = desc.find(d => d.hasOwnProperty('__text')) || { '__text': '' };
+      output.description = output.description.__text;
+    }
+  } else if (typeof desc === 'string') {
+    output.description = desc;
+  }
+
+  output.description = sanitizeHtml(output.description, { allowedTags: [] });
+
+  return output;
+}
 
 app.listen(8080, () => console.log('Listening on 8080'));
--- src/podcast/PodcastContent.js
+++ src/podcast/PodcastContent.js
@@ -1,32 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { format } from 'date-fns';
+import { Button, Tag } from '@blueprintjs/core';
 import styles from './PodcastContent.module.scss';
+
+const formatDate = (date) => format(date, 'MM.DD.YYYY');
 
 export default function PodcastContent({ feed }) {
   const [isLoading, setIsLoading] = useState(false);
   const [content, setContent] = useState(null);
 
   function renderPodcastContent() {
+    const episodeList = (
+      <ul className={styles.list}>
+        {content && content.episodes.map(ep => (
+          <li key={ep.id} className={styles.episode}>
+            <Tag round={true} minimal={true}>{formatDate(ep.pubDate)}</Tag>
+            <a
+              href={ep.link}
+              className={styles.episodeTitle}
+              target="_blank">{ep.title}</a>
+            {/* <span className={styles.episodeSummary}>{ep.summary}</span> */}
+            <div className={styles.episodeControls}>
+              <Button icon="play" />
+            </div>
+          </li>
+        ))}
+
+        {isLoading && !content && [0, 1, 2].map(n => (
+          <li key={n} className={`bp3-skeleton ${styles.episode}`}>&nbsp;</li>
+        ))}
+      </ul>
+    );
+
     return (
       <div>
         <div className={styles.heading}>
-          {content && <img src={getImage(content.rss.channel.image)} alt={content.rss.channel.title} />}
-          <h2>
-            <span className={isLoading ? 'bp3-skeleton' : undefined}>
+          <div className={isLoading ? `bp3-skeleton ${styles.image}` : styles.image}>
+            {content && <img src={content.image} alt={content.title} />}
+          </div>
+
+          <div className={styles.description}>
+            <h2>
+              <span className={isLoading ? 'bp3-skeleton' : undefined}>
+                {content
+                  ? content.title
+                  : 'Lorem ipsum'}
+              </span>
+            </h2>
+  
+            <p className={isLoading ? 'bp3-skeleton' : undefined}>
               {content
-                ? content.rss.channel.title
-                : 'Lorem ipsum'}
-            </span>
-          </h2>
+                ? content.description
+              : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget tortor felis. Fusce dapibus metus in dapibus mollis. Quisque eget ex diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget tortor felis. Fusce dapibus metus in dapibus mollis. Quisque eget ex diam.'}
+            </p>
+          </div>
         </div>
 
-        <p className={isLoading ? 'bp3-skeleton' : undefined}>
-          {content
-            ? content.rss.channel.description
-          : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget tortor felis. Fusce dapibus metus in dapibus mollis. Quisque eget ex diam.'}
-        </p>
-
-        <div></div>
+        <h3>Episodes</h3>
+        <div>
+          {episodeList}
+        </div>
       </div>
     );
   }

1eb816516b6030204444f90a4eb7dc199df29166
Ross J <ross.joo@gmail.com>
2019-06-27 10:11:15 -0400

    Reorg PodcastContent

--- src/podcast/PodcastContent.js
+++ src/podcast/PodcastContent.js
@@ -13,70 +13,11 @@ export default function PodcastContent({ feed }) {
 
   const handleLimitIncrease = () => setEpisodeLimit(episodeLimit + 50);
 
-  function renderPodcastContent() {
-    const episodeList = (
-      <ul className={styles.list}>
-        {content && content.episodes.slice(0, episodeLimit).map(ep => (
-          <li key={ep.id} className={styles.episode}>
-            <Tag round={true} minimal={true}>{formatDate(ep.pubDate)}</Tag>
-            <a
-              href={ep.link}
-              className={styles.episodeTitle}
-              target="_blank"
-              rel="noopener noreferrer">{ep.title}</a>
-            {/* <span className={styles.episodeSummary}>{ep.summary}</span> */}
-            {ep.media && <div className={styles.episodeControls}>
-              <Button icon="play" />
-            </div>}
-          </li>
-        ))}
-
-        {(content && content.episodes.length > episodeLimit) && (<li><Button onClick={handleLimitIncrease}>Show more</Button></li>)}
-
-        {isLoading && !content && [0, 1, 2].map(n => (
-          <li key={n} className={`bp3-skeleton ${styles.episode}`}>&nbsp;</li>
-        ))}
-      </ul>
-    );
-
-    return (
-      <div>
-        <div className={styles.heading}>
-          <div className={isLoading ? `bp3-skeleton ${styles.image}` : styles.image}>
-            {content && <img src={content.image} alt={content.title} />}
-          </div>
-
-          <div className={styles.description}>
-            <h2>
-              <span className={isLoading ? 'bp3-skeleton' : undefined}>
-                {content
-                  ? content.title
-                  : 'Lorem ipsum'}
-              </span>
-            </h2>
-  
-            <p className={isLoading ? 'bp3-skeleton' : undefined}>
-              {content
-                ? content.description
-              : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget tortor felis. Fusce dapibus metus in dapibus mollis. Quisque eget ex diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget tortor felis. Fusce dapibus metus in dapibus mollis. Quisque eget ex diam.'}
-            </p>
-          </div>
-        </div>
-
-        <h3>Episodes</h3>
-        <div>
-          {episodeList}
-        </div>
-      </div>
-    );
-  }
-
   useEffect(() => {
     if (feed) {
       async function getFeedContent() {
         try {
           setIsLoading(true);
-
           const response = await axios.post('http://localhost:8080/api/feed', {
             feed
           });
@@ -95,9 +36,64 @@ export default function PodcastContent({ feed }) {
     }
   }, [feed]);
 
+  const episodeList = (
+    <ul className={styles.list}>
+      {content && content.episodes.slice(0, episodeLimit).map(ep => (
+        <li key={ep.id} className={styles.episode}>
+          <Tag round={true} minimal={true}>{formatDate(ep.pubDate)}</Tag>
+          <a
+            href={ep.link}
+            className={styles.episodeTitle}
+            target="_blank"
+            rel="noopener noreferrer">{ep.title}</a>
+          {/* <span className={styles.episodeSummary}>{ep.summary}</span> */}
+          {ep.media && (
+            <div className={styles.episodeControls}>
+              <Button icon="play" />
+            </div>
+          )}
+        </li>
+      ))}
+
+      {(content && content.episodes.length > episodeLimit) && (
+        <li><Button onClick={handleLimitIncrease}>Show more</Button></li>
+      )}
+
+      {isLoading && !content && [0, 1, 2].map(n => (
+        <li key={n} className={`bp3-skeleton ${styles.episode}`}>&nbsp;</li>
+      ))}
+    </ul>
+  );
+
   return (
     <section className={styles.section}>
-      {renderPodcastContent()}
+      <div className={styles.heading}>
+        <div className={isLoading ? `bp3-skeleton ${styles.image}` : styles.image}>
+          });
+          {content && <img src={content.image} alt={content.title} />}
+        </div>
+
+        <div className={styles.description}>
+          <h2>
+            <span className={isLoading ? 'bp3-skeleton' : undefined}>
+              {content
+                ? content.title
+                : 'Lorem ipsum'}
+            </span>
+          </h2>
+
+          <p className={isLoading ? 'bp3-skeleton' : undefined}>
+            {content
+              ? content.description
+              : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget tortor felis. Fusce dapibus metus in dapibus mollis. Quisque eget ex diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget tortor felis. Fusce dapibus metus in dapibus mollis. Quisque eget ex diam.'}
+          </p>
+        </div>
+      </div>
+
+      <h3>Episodes</h3>
+      <div>
+        {episodeList}
+      </div>
     </section>
   );
 }

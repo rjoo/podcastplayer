a8c6ac25d143febce46a617292089e59b0f1f46f
Ross J <ross.joo@gmail.com>
2019-06-28 15:26:13 -0400

    Add a notif. toaster

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Toaster, Position } from '@blueprintjs/core';
 import Home from './Home';
 import Search from './search/Search';
 import TopBar from './topbar/TopBar';
@@ -6,8 +8,25 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 
 function App() {
+  const { episodeTitle } = useSelector(state => state.podcast);
+  let toaster;
+
+  const handleToasterRef = ref => toaster = ref;
+
+  useEffect(() => {
+    if (episodeTitle)
+      toaster.show({
+        message: (<span>Now listening to <strong>{episodeTitle}</strong></span>),
+        timeout: 3000,
+        icon: 'music',
+        intent: 'primary'
+      });
+  }, [episodeTitle, toaster]);
+
   return (
     <div className="App">
+      <Toaster position={Position.TOP_LEFT} ref={handleToasterRef} />
+
       <HashRouter>
         <TopBar />
 
